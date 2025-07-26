5b9d5430e83f1968680f95bfb328f486c18056cd
Mohadese Faraji <realmhdse@gmail.com>
2025-07-26 23:45:40 +0330

    make routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
 import react from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {  QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 const App=()=>{
+  const queryclient= new QueryClient()
   return(
-    <div>sdddd</div>
+    <div>
+      <QueryClientProvider client={queryclient}>
+<ReactQueryDevtools/>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage/>}>
+        <Route path="/header" element={<Header/>}/>
+        </Route>
+        <Route path="/products" element={<Products/>}/>
+        <Route path="" element={<NotFound/>}/>
+      </Routes>
+      </BrowserRouter>
+      </QueryClientProvider>
+    </div>
   )
 }
 export default App
